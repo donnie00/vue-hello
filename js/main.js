a3d1506a6c339cfface0b47be59b47bb4ab12d19
ippoRulez <ippolitodonato@gmail.com>
2022-11-21 16:59:01 +0100

    Added name dinamically and function to change img on click

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue;
+
+createApp({
+	data() {
+		return {
+			nome: 'Donato',
+			cognome: 'Ippolito',
+			src: 'https://picsum.photos/400/200',
+			random: 1,
+		};
+	},
+
+	methods: {
+		imgChange() {
+			this.src = `https://picsum.photos/400/200?random=${this.random}`;
+			this.random++;
+		},
+	},
+}).mount('#app');
